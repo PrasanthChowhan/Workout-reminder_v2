93426dbc625732b8dd245033df9a706e7710a2e6
prasanth chowhan <prashanth.chawhan@gmail.com>
2026-08-15 21:28:16 +0530

    fix: toast.warning is not a function and bump version to 0.1.4

--- src/utils/toast.js
+++ src/utils/toast.js
@@ -15,4 +15,5 @@ export const toast = {
   success: (message) => showToast(message, "success"),
   error: (message) => showToast(message, "error"),
   info: (message) => showToast(message, "info"),
+  warning: (message) => showToast(message, "warning"),
 };
